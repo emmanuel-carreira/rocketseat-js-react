5fa5ee9819e3dbfbd515ba933be07b59cd252bec
Emmanuel Alves <emmanuelalves@locus.software>
2019-10-17 15:05:24 -0300

    Added remove todo function

--- Javascript/Module 3/index.js
+++ Javascript/Module 3/index.js
@@ -16,9 +16,16 @@ function renderTodos() {
     for (todo of todos) {
         var todo_element = document.createElement('li');
         var todo_text = document.createTextNode(todo);
+        var link_element = document.createElement('a');
+        var link_text = document.createTextNode('Exclude');
+        var index = todos.indexOf(todo);
+        link_element.setAttribute('href', '#');
+        link_element.setAttribute('onclick', 'deleteTodo(' + index + ')')
+        link_element.appendChild(link_text);
         
         list_element.appendChild(todo_element);
         todo_element.appendChild(todo_text);
+        todo_element.appendChild(link_element);
     }
 }
 
@@ -26,6 +33,11 @@ function addTodo() {
     var todo_text = input_element.value;
     todos.puch(todo_text);
     input_element.value = '';
+    renderTodos();
+}
+
+function deleteTodo(pos) {
+    todos.splice(pos, 1);
     renderTodos();
 }
 

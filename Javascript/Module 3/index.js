c137318b83d2130910d69765278dc893908a6bda
Emmanuel Alves <emmanuelalves@locus.software>
2019-10-17 14:02:00 -0300

    Adapted list od todos to js

---
+++ Javascript/Module 3/index.js
@@ -0,0 +1,9 @@
+var list_element = document.querySelector('#app ul');
+var input_element = document.querySelector('#app input');
+var button_element = document.querySelector('#app button');
+
+var todos = [
+    'Make coffee',
+    'Study Javascript',
+    'Access Rocketseat community'
+]

083af5e24a40f6d316c7d8ead2351d9b2e1ebbb3
Emmanuel Alves <emmanuelalves@locus.software>
2019-10-18 17:31:27 -0300

    Finished exercise 2

--- Javascript/Module 4/challenge.js
+++ Javascript/Module 4/challenge.js
@@ -20,4 +20,32 @@ checkAge(20)
         console.log(error)
     });
 //Exercise 2
+var username = document.querySelector('input');
+var submit_button = document.querySelector('button');
+
+submit_button.onclick = getRepos;
+    
+function getRepos() {
+    var url = 'https://api.github.com/users/' + username.value +'/repos'
+    username.value = '';
+
+    axios.get(url)
+        .then(function(response) {
+            for(var i = 0; i < response.data.length; i++){
+                var repo = response.data[i];
+                addRepo(repo.name);
+            }
+        })
+        .catch(function(error) {
+            alert(error);
+        });
+};
+
+function addRepo(repo_name) {
+    var list = document.querySelector('ul');
+    var list_element = document.createElement('li');
+    list_element.textContent = repo_name;
+    list.appendChild(list_element);
+}
+
 //Exercise 3

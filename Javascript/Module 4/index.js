a852958e3587965e0f7b222f7a32d829b673e925
Emmanuel Alves <emmanuelalves@locus.software>
2019-10-18 16:31:37 -0300

    Messing with axios request

--- Javascript/Module 4/index.js
+++ Javascript/Module 4/index.js
@@ -1,21 +1,7 @@
-var my_promise = function() {
-    return new Promise(function(resolve, reject) {
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'https://api.github.com/users/emmanuel-carreira');
-        xhr.send(null);
-        xhr.onreadystatechange = function() {
-            if(xhr.readyState === 4) {
-                if(xhr.status === 200) {
-                    resolve(JSON.parse(xhr.responseText));
-                }
-                else {
-                    reject('Request error');
-                }
-            }
-        }        
+axios.get('https://api.github.com/users/emmanuel-carreira')
+    .then(function(response) {
+        console.log(response);
+    })
+    .catch(function(error) {
+        console.warn(error);
     });
-};
-
-my_promise()
-    .then(function(response) {})
-    .catch(function(error) {});

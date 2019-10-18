46ecbcc59cc36b1db64cae5ab7bf811633b6d7d5
Emmanuel Alves <emmanuelalves@locus.software>
2019-10-18 16:27:06 -0300

    Messing with promise

--- Javascript/Module 4/index.js
+++ Javascript/Module 4/index.js
@@ -1,10 +1,21 @@
-var xhr = new XMLHttpRequest();
+var my_promise = function() {
+    return new Promise(function(resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://api.github.com/users/emmanuel-carreira');
+        xhr.send(null);
+        xhr.onreadystatechange = function() {
+            if(xhr.readyState === 4) {
+                if(xhr.status === 200) {
+                    resolve(JSON.parse(xhr.responseText));
+                }
+                else {
+                    reject('Request error');
+                }
+            }
+        }        
+    });
+};
 
-xhr.open('GET', 'https://api.github.com/users/emmanuel-carreira');
-xhr.send(null);
-
-xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4){
-        console.log(JSON.parse(xhr.responseText));
-    }
-}
+my_promise()
+    .then(function(response) {})
+    .catch(function(error) {});

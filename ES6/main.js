66765ec4d7362668949beea51f6b562bb3bbc816
Emmanuel Alves <emmanuelalves@locus.software>
2019-10-18 19:07:23 -0300

    Messing with classes

--- ES6/main.js
+++ ES6/main.js
@@ -1,1 +1,16 @@
-alert('test');
+class TodoList {
+    constructor() {
+        this.todos = [];
+    }
+
+    addTodo() {
+        this.todos.push('New todo');
+        console.log(this.todos);
+    }
+}
+
+const my_list = new TodoList();
+
+document.getElementById('newtodo').onclick = function() {
+    my_list.addTodo();
+}

0a31cadd231d15543bc7bb8efd6f5eec25b15d87
Emmanuel Alves <emmanuelalves@locus.software>
2019-10-18 16:44:56 -0300

    Finished exercise 1

--- Javascript/Module 4/challenge.js
+++ Javascript/Module 4/challenge.js
@@ -1,3 +1,23 @@
 //Exercise 1
+var checkAge = function(age) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(() => {
+            if(age >= 18) {
+                resolve('User is more than 18 years old');
+            }
+            else {
+                reject('User is underage');
+            }
+        }, 2000);
+    });
+};
+
+checkAge(20)
+    .then(function(response) {
+        console.log(response)
+    })
+    .catch(function(error) {
+        console.log(error)
+    });
 //Exercise 2
 //Exercise 3

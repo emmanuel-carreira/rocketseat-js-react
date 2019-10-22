ff4a44ddf0a969ab6daedefdd49fd26cb45f3626
Emmanuel Alves <emmanuelalves@locus.software>
2019-10-22 12:54:17 -0300

    Finished exercise 2

--- ES6/Module1/Challenge1.js
+++ ES6/Module1/Challenge1.js
@@ -28,3 +28,20 @@ const admin = new Admin('unclescrooge@walt.disney', 'mypreciousgold');
 
 console.log(user.isAdmin());
 console.log(admin.isAdmin());
+
+//Exercise 2
+const users = [
+    { name: 'Diego', age: 23, company: 'Rocketseat' },
+    { name: 'Gabriel', age: 15, company: 'Rocketseat' },  
+    { name: 'Lucas', age: 30, company: 'Facebook' },
+];
+
+const ages = users.map((user) => user.age);
+const adults_rocketseat = users.filter( (user) => (user.age >= 18) && 
+                                        (user.company === "Rocketseat")
+                                      );
+const googlers = users.find((user) => user.company === "Google");
+
+console.log(ages);
+console.log(adults_rocketseat);
+console.log(googlers);
